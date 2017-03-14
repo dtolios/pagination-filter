1c51625fcc8cca0218f025b7ab45947c06a7ae6c
Dimitri Tolios <dtolios09@gmail.com>
2017-03-14 00:07:20 -0700

    Modularized Pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,34 +1,63 @@
-// Useful Constants for calculating pagination
-const $students = $('.student-list').children();
-const numStudents = $students.length;
-const numPages = Math.ceil( numStudents / 10 );
+let Pagination = ( () => {
+	const $page = $('.page');
 
-// Initially only show ten students
-$('.student-item:gt(9)').hide();
+	let studentsPerPage = 10;
+	let numStudents = $('.student-list').children().length;
+	let numPages = Math.ceil( numStudents / studentsPerPage );
 
-// Add pagination div
-$('.page').append('<div class="pagination"><ul></ul></div>');
+	let addPaginationContainer = () => {
+		$paginationContainer = $('<div class="pagination"><ul></ul></div>');
+		$page.append($paginationContainer);
+	};
 
-// Add pagination buttons
-for(let i = 1; i <= numPages; i++) {
-	if(i === 1)
-		var $button = $('<li><a href="#" class="active">' + i + '</a></li>');
-	else
-		var $button = $('<li><a href="#">' + i + '</a></li>');
-	$('.pagination ul').append($button);
-}
+	let addPageButton = (number) => {
+		$button = $('<li><a href="#">' + number + '</a></li>');
+		$('.pagination ul').append($button);
+	};
 
-// Click event for loading appropriate subset of students
-$('.pagination ul').on('click', 'li', ( event ) => {
-	let $target = $(event.target);
-	let endIndex = parseInt($target.text()) * 10;
-	let startIndex = endIndex - 10;
+	let init = () => {
+		// Show initial students
+		$('.student-item:gt(' + (studentsPerPage - 1) + ')').hide();
 
-	// Set active button
-	$('.pagination ul li').find('.active').removeClass('active');
-	$target.addClass('active');
-	// Show correct range of students
-	$('.student-item').hide();
-	$('.student-item').slice(startIndex, endIndex).show();
-});
+		addPaginationContainer();
 
+		// Add pagination buttons
+		for(let i = 1; i <= numPages; i++) {
+			addPageButton(i);
+		}
+
+		// Set button 1 to active
+		$('.pagination ul li:first-child a').addClass('active');
+
+		// Add event listener
+		$('.pagination ul').on('click', 'li', buttonClick);
+	};
+
+	let setActive = ($target) => {
+		// Set active button
+		$('.pagination ul li').find('.active').removeClass('active');
+		$target.addClass('active');
+	};
+
+	let showContent = (start, end) => {
+		// Show correct range of students
+		$('.student-item').hide();
+		$('.student-item').slice(start, end).show();
+	}; 
+
+	let buttonClick = (ev) => {
+		ev.preventDefault();
+		$target = $(ev.target);
+		setActive($target);
+		let end = parseInt($target.text()) * 10;
+		let start = end - 10;
+		showContent(start, end);
+	};
+
+	return {
+		init: init
+	}
+
+})();
+
+Pagination.init();
