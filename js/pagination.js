b46a5ad80d37fb074b85dc6e9033e027dee46949
Dimitri Tolios <dtolios@riotgames.com>
2017-03-14 10:45:40 -0700

    Code refactoring, comments cleanup

--- js/pagination.js
+++ js/pagination.js
@@ -1,20 +1,55 @@
+// Pagination module for enabling pagination functionality a Student List page
 let Pagination = ( () => {
-	const $page = $('.page');
+	
+	// PRIVATE PROPERTIES: //
 
+	const $page = $('.page');
 	let studentsPerPage = 10;
 	let numStudents = $('.student-list').children().length;
 	let numPages = Math.ceil( numStudents / studentsPerPage );
+	
+	//  PRIVATE METHODS: //
 
+	// EFFECTS: adds required html container for pagination buttons
 	let addPaginationContainer = () => {
 		$paginationContainer = $('<div class="pagination"><ul></ul></div>');
 		$page.append($paginationContainer);
 	};
 
+	// EFFECTS: adds a page button to the pagination container
 	let addPageButton = (number) => {
 		$button = $('<li><a href="#">' + number + '</a></li>');
 		$('.pagination ul').append($button);
 	};
 
+	// EFFECTS: sets the target class to "active"
+	let setActive = ($target) => {
+		// Set active button
+		$('.pagination ul li').find('.active').removeClass('active');
+		$target.addClass('active');
+	};
+
+	// EFFECTS: displays the elements between "start" and "end"
+	let showContent = (start, end) => {
+		// Show correct range of students
+		$('.student-item').hide();
+		$('.student-item').slice(start, end).show();
+	}; 
+
+	// EFFECTS: event handler for page button clicks
+	let buttonClick = (ev) => {
+		ev.preventDefault();
+		$target = $(ev.target);
+		setActive($target);
+		let end = parseInt($target.text()) * 10;
+		let start = end - 10;
+		showContent(start, end);
+	};
+
+
+	// PUBLIC METHODS //
+
+	// EFFECTS: initializes pagination functionality
 	let init = () => {
 		// Show initial students
 		$('.student-item:gt(' + (studentsPerPage - 1) + ')').hide();
@@ -33,36 +68,15 @@ let Pagination = ( () => {
 		$('.pagination ul').on('click', 'li', buttonClick);
 	};
 
+	// EFFECTS: removes pagination html and functionality
 	let destroy = () => {
 		$('.pagination').remove();
 	};
 
-	let setActive = ($target) => {
-		// Set active button
-		$('.pagination ul li').find('.active').removeClass('active');
-		$target.addClass('active');
-	};
-
-	let showContent = (start, end) => {
-		// Show correct range of students
-		$('.student-item').hide();
-		$('.student-item').slice(start, end).show();
-	}; 
-
-	let buttonClick = (ev) => {
-		ev.preventDefault();
-		$target = $(ev.target);
-		setActive($target);
-		let end = parseInt($target.text()) * 10;
-		let start = end - 10;
-		showContent(start, end);
-	};
-
+	// PUBLIC API: //
 	return {
 		init: init,
 		destroy: destroy
 	}
 
 })();
-
-Pagination.init();
--- js/search.js
+++ js/search.js
@@ -1,4 +1,9 @@
+// Search module for enabling search on a Student List page
 let Search = ( () => {
+	
+	// PRIVATE METHODS: //
+
+	// EFFECTS: Appends the required search html to the supplied element
 	let addSearch = (el) => {
 		let searchHTML = '<div class="student-search">\
 							<input placeholder="Search for students...">\
@@ -7,24 +12,29 @@ let Search = ( () => {
 		$(el).append(searchHTML);
 	};
 
+	// EFFECTS: resets the visibility for all students and re-initializes pagination
 	let reset = () => {
 		$('.student-item').show();
 		Pagination.init();
 	};
 
+	// EFFECTS: returns the user input from the search field
 	let getQuery = () => {
 		return $('.student-search input').val();
 	};
 
+	// EFFECTS: appends an error message as a list item to the student list
 	let appendError = () => {
 		let $error = $('<li id="search-error">No Matching Students Found</li>');
 		$('.student-list').append($error);
 	};
 
+	// EFFECTS: removes the error message from the student list
 	let removeError = () => {
 		$('#search-error').remove();
 	};
 
+	// EFFECTS: Toggles the visibility of student items based on the query contents
 	let filterStudents = () => {
 		let query = getQuery().toUpperCase();
 
@@ -47,21 +57,24 @@ let Search = ( () => {
 		}
 	};
 
+	// EFFECTS: Event handler for searching
 	let searchExe = (ev) => {
 		ev.preventDefault();
 		Pagination.destroy();
 		filterStudents();
 	};
 
+	// PUBLIC METHODS: //
+
+	// EFFECTS: Initializing search functionality by adding proper HTML and adding event listeners
 	let init = () => {
 		addSearch('.page-header');
 		$('.student-search button').on('click', searchExe);
 		$('.student-search input').on('keyup', (e) => { if(e.which === 13) searchExe(e); });
 	};
 
+	// PUBLIC API: //
 	return {
 		init: init
 	};
 })();
-
-Search.init();
